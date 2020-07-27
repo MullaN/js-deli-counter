c0ea20412bf2903d9f676e246b8d72ce7ca7edc5
Andrew Mullan <admullan@gmail.com>
2020-07-27 16:34:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  var beingServed
+  if (katzDeliLine.length > 0) {
+    beingServed = `Currently serving ${katzDeliLine[0]}.`
+    katzDeliLine.shift()
+  } else {
+    beingServed = "There is nobody waiting to be served!"
+  }
+  return beingServed
+}
+
+function currentLine(katzDeliLine) {
+  var fullLine
+  if (katzDeliLine.length > 0) {
+    fullLine = "The line is currently:"
+    for (let i = 0; i < katzDeliLine.length; i += 1) {
+      fullLine = fullLine + ` ${i + 1}. ${katzDeliLine[i]}`
+      if (i + 1 < katzDeliLine.length) {
+        fullLine = fullLine + ","
+      }
+    }
+  } else {
+    fullLine = "The line is currently empty."
+  }
+  return fullLine
+}
